50ee61837964431e4936c71a1874ff35f5a6f330
Mohamed Elweza <100067756+MohamedElweza@users.noreply.github.com>
2022-10-13 18:16:57 +0200

    Add Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -30,73 +31,84 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    List<Books> books =[];
+    List<Books> books = [];
     return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async{
-            TextEditingController authorinput = TextEditingController();
-            TextEditingController nameinput = TextEditingController();
-            TextEditingController urlinput = TextEditingController();
-            await showModalBottomSheet(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(15.0),
-                  ),
+        child: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          TextEditingController authorinput = TextEditingController();
+          TextEditingController nameinput = TextEditingController();
+          TextEditingController urlinput = TextEditingController();
+          await showModalBottomSheet(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(15.0),
                 ),
-                context: context, builder: (context){
-                  return SizedBox(
-                    height: 270,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            children: [
-                              TextField(
-                                controller: nameinput,
-                                decoration:
-                                const InputDecoration(label: Text('Book Title')),
-                              ),
-                              TextField(
-                                controller: authorinput,
-                                decoration:
-                                const InputDecoration(label: Text('Book Author')),
-                              ),
-                              TextField(
-                                controller: urlinput,
-                                decoration:
-                                const InputDecoration(label: Text('Book Cover URL')),
-                              ),
-                            ],
+              ),
+              context: context,
+              builder: (context) {
+                return SizedBox(
+                  height: 270,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            TextField(
+                              controller: nameinput,
+                              decoration: const InputDecoration(
+                                  label: Text('Book Title')),
+                            ),
+                            TextField(
+                              controller: authorinput,
+                              decoration: const InputDecoration(
+                                  label: Text('Book Author')),
+                            ),
+                            TextField(
+                              controller: urlinput,
+                              decoration: const InputDecoration(
+                                  label: Text('Book Cover URL')),
+                            ),
+                          ],
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(500, 50),
+                            backgroundColor: Colors.blue,
                           ),
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                minimumSize:const Size (500,50),
-                                  backgroundColor: Colors.blue,
-                                  ),
-                              onPressed: (){
-                                BooksProvider.instance.insertBook(Books(
-                                    name: nameinput.text,
-                                    author: authorinput.text,
-                                    image: urlinput.text,
-                                    ));
-                                print(books);
-                                Navigator.pop(context);
-                              }, child: const Text('ADD', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),)
-                        ],
-                      ),
+                          onPressed: () {
+                            BooksProvider.instance.insertBook(Books(
+                              name: nameinput.text,
+                              author: authorinput.text,
+                              image: urlinput.text,
+                            ));
+                            print(books);
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            'ADD',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
                     ),
-                  );
-            });
-          },child: const Icon(Icons.add, size: 50,),
+                  ),
+                );
+              });
+        },
+        child: const Icon(
+          Icons.add,
+          size: 50,
         ),
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Available Books'),
-        ),
-        body: FutureBuilder<List<Books>>(
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Available Books'),
+      ),
+      body: FutureBuilder<List<Books>>(
           future: BooksProvider.instance.getAllBooks(),
           builder: (context, snapshot) {
             if (snapshot.hasError) {
@@ -114,17 +126,53 @@ class _HomeState extends State<Home> {
                 return Padding(
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
                   child: ListTile(
-                    leading: Container(height:50, width:50,child: Image.network(book.image, fit: BoxFit.cover)),
-                    trailing: IconButton(onPressed: () {
-                      if (book.id != null) {
-                         BooksProvider.instance.deleteBook(book.id!);
-                      }
-                      print(books);
-                      setState(() {});
-                    }, icon: const Icon(Icons.delete_forever),),
+                    leading: Container(
+                        height: 50,
+                        width: 50,
+                        child: Image.network(book.image, fit: BoxFit.cover)),
+                    trailing: IconButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text(
+                                  'Delete Book',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                content: const Text(
+                                    'Are you sure you want to delete this book ?'),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("Cancel")),
+                                  TextButton(
+                                    onPressed: () {
+                                      if (book.id != null) {
+                                        BooksProvider.instance
+                                            .deleteBook(book.id!);
+                                      }
+                                      print(books);
+                                      setState(() {});
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("Yes",
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                        )),
+                                  )
+                                ],
+                              );
+                            });
+                      },
+                      icon: const Icon(Icons.delete_forever),
+                    ),
                     title: Text(
-                      book.name, style: const TextStyle(fontWeight: FontWeight
-                        .bold),),
+                      book.name,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
                     subtitle: Text(book.author),
                   ),
                 );
